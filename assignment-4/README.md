881fb27c6241badff0133fdf7a4e0d6238aad396
ruspe <79607958+ruspe@users.noreply.github.com>
2022-10-14 16:10:13 -0400

    Assignment 4

---
+++ assignment-4/README.md
@@ -0,0 +1,3 @@
+incrementVersion.py takes a file name and increments it by 1. 
+The file format is this: {project}.{asset}.{task}.{artist}.{version}.{ext}
+It takes no arguments. 
---
+++ assignment-4/python/incrementVersion.py
@@ -0,0 +1,29 @@
+"""
+this script should take a file with proper naming structure and increment the version by 1
+
+for use in maya, either make it its own shelf button or copy directly into the script editor
+
+"""
+
+import maya.cmds as cmds
+
+#get current file name
+currentFileName = cmds.file(q=True, sn=True, shn=True)
+
+#splits file name
+project, asset, task, artist, version, ext = currentFileName.split(".")
+saveFileFormat = "{project}.{asset}.{task}.{artist}.{version}.{ext}"
+
+#moves split file name into its own dict
+assetInfo = {
+"project": project,
+"asset": asset,
+"task": task,
+"artist": artist,
+"version": int(version) + 1, #increment the version
+"ext": ext
+}
+
+#rename and save
+cmds.file(rename=saveFileFormat.format(**assetInfo))
+cmds.file(save=True)
