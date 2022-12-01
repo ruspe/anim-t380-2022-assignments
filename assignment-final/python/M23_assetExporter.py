028d085714d7ae6381309cb80df85706b988e744
ruspe <79607958+ruspe@users.noreply.github.com>
2022-12-01 11:01:01 -0500

    Added project location
    settings needs to be moved to a standard location

--- assignment-final/python/M23_assetExporter.py
+++ assignment-final/python/M23_assetExporter.py
@@ -3,17 +3,23 @@ asset exporter
 
 '''
 import maya.cmds as cmds
+import os
+import json
+
+settings = json.load("C:/Users/cypek/Documents/anim-t380-2022-assignments/assignment-final/python/settings.json") #move to standard location
 
 #asset information for naming - should be inputted by user through UI
 assetInfo = {
 "asset": "testAsset",
 "location": "basement", #location is either basement, groundFloor, or secondFloor
 "version": 1,
+"projectDirectory": settings.get("projectDirectory")
 } #temp values
 
 
 #formatting the file path - how to get file path from user?
-filePathFormat = "C:/Users/cypek/Documents/Motel23Studios/ShadyCreekLodge/Content/ShadyCreekLodge/Assets/Environment/{location}/{asset}_{location}_V{version}.fbx"
+#filePathFormat = "C:/Users/{user}/Documents/Motel23Studios/ShadyCreekLodge/Content/ShadyCreekLodge/Assets/Environment/{location}/{asset}_{location}_V{version}.fbx"
+filePathFormat = "{projectDirectory}/ShadyCreekLodge/Content/ShadyCreekLodge/Assets/Environment/{location}/{asset}_{location}_V{version}.fbx"
 
 #Freeze transforms
 cmds.makeIdentity(a=True)
