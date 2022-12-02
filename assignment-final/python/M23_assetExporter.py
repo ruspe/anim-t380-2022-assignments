7ebe164f7ef2a2d132c66f3b51ebd9137a469c13
ruspe <79607958+ruspe@users.noreply.github.com>
2022-12-02 15:24:47 -0500

    added UV checking

--- assignment-final/python/M23_assetExporter.py
+++ assignment-final/python/M23_assetExporter.py
@@ -6,6 +6,21 @@ import maya.cmds as cmds
 import os
 import json
 
+#function for checking UVs
+#works by checking the history of the object for anything UV related that has been done - does not work if history has already been deleted
+def checkUVs():
+    UVHistory = ["polyTweak", "polyMapSewMove"] #should be a list of all UV related commands
+
+    objHistory = cmds.listHistory()
+    print(objHistory)
+
+    for ele in UVHistory:
+        if any(ele in s for s in objHistory):
+            print("yes UVs")  #UVs have been done - tool can continue
+        else:
+            print("no UVs") # pop up here - "There is no UV history on this object. Fix now?" with Fix (brings up UV editor) or Continue Anyway (continues tool)
+
+
 #get settings from program files - user needs to copy file here
 f = open("C:/Program Files/settings.json")
 
@@ -19,19 +34,17 @@ assetInfo = {
 "projectDirectory": settings.get("projectDirectory")
 } #temp values
 
-
-#formatting the file path - how to get file path from user?
-#filePathFormat = "C:/Users/{user}/Documents/Motel23Studios/ShadyCreekLodge/Content/ShadyCreekLodge/Assets/Environment/{location}/{asset}_{location}_V{version}.fbx"
+#file path
 filePathFormat = "{projectDirectory}/ShadyCreekLodge/Content/ShadyCreekLodge/Assets/Environment/{location}/{asset}_{location}_V{version}.fbx"
+
+#check UVs
+checkUVs()
 
 #Freeze transforms
 cmds.makeIdentity(a=True)
 
 #delete history
 cmds.delete(constructionHistory = True)
-
-#check UVs
-
 
 #export file
 cmds.file(filePathFormat.format(**assetInfo), force=True, exportSelected = True, type = 'FBX export')
